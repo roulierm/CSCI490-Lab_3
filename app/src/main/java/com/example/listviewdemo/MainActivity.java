878ea25d05f8886bfc49a618358ec387b7ab399c
Melissa R <roulierm@users.noreply.github.com>
2021-03-13 18:21:41 -0500

    second commit

--- app/src/main/java/com/example/listviewdemo/MainActivity.java
+++ app/src/main/java/com/example/listviewdemo/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.listviewdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+//import android.content.Intent;
 import android.os.Bundle;
+//import android.widget.Adapter;
+//import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] classes = {"Discrete Structures", "Digital Logic & Comp Org", "Database Concepts", "German 201", "Public Speaking"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, classes);
+
+        listView = findViewById(R.id.class_schedule);
+        listView.setAdapter(adapter);
+
+
     }
 }
